5b28a90e76fddac6827e1c62d459acc2200cec4f
nicordev <nicolas.renvoise.dev@gmail.com>
2019-10-20 14:32:18 +0200

    Show a list of RSS feeds

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\NewsFetcher;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,8 +12,15 @@ class NewsController extends AbstractController
      * @Route("/news", name="news_index")
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(NewsFetcher $newsFetcher)
     {
-        return $this->render('news/index.html.twig');
+        $maxNewsCount = 3;
+        $rssFeeds = [
+            $newsFetcher->fetchRssFeed("https://afup.org/pages/site/rss.php", $maxNewsCount),
+            $newsFetcher->fetchRssFeed("https://www.alsacreations.com/rss/actualites.xml", $maxNewsCount),
+            $newsFetcher->fetchRssFeed("https://putaindecode.io/feed.xml", $maxNewsCount)
+        ];
+
+        return $this->render('news/index.html.twig', ["rssFeeds" => $rssFeeds]);
     }
 }
