b5a63c92a180e787d8a7c859ca54164385f88688
nicordev <nicolas.renvoise.dev@gmail.com>
2019-10-21 17:07:31 +0200

    Add SonarCloud analysis, add phpstan, add phpcodesniffer

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,10 +14,10 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-         if ($this->getUser()) {
-             $this->addFlash("notice", "You are already authenticated.");
-             return $this->redirectToRoute('news_list');
-         }
+        if ($this->getUser()) {
+            $this->addFlash("notice", "You are already authenticated.");
+            return $this->redirectToRoute('news_list');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
